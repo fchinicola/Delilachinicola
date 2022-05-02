5feb357bf62a60418a4b59aa26b20890137f0970
fchinicola <fchinicola@gmail.com>
2022-05-02 17:55:36 -0300

    cookie session fix

--- src/routes/auth/google.js
+++ src/routes/auth/google.js
@@ -1,46 +1,75 @@
-const express = require('express');
-
+const express = require("express");
 const router = express.Router();
-const passport = require('passport');
-const jwt = require('jsonwebtoken');
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
 
 const { JWT_SECRET } = process.env;
-const cors = require('cors');
+const cors = require("cors");
 
-const Session = require('cookie-session');
+const cookieSession = require("cookie-session");
 
 router.use(cors());
 
 router.use(
-  Session({
-    name: 'google-auth-session',
-    keys: ['key1', 'key2'],
-  }),
+  cookieSession({
+    name: "google-auth-session",
+    keys: ["key1", "key2"],
+  })
 );
 
 router.use(passport.initialize());
 router.use(passport.session());
 
-router.get('/logout', (req, res) => {
+router.get("/logout", (req, res) => {
   req.session = null;
   req.logout();
-  res.redirect('/');
+  res.redirect("/");
 });
 
 router.get(
-  '/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email'],
-    prompt: 'consent',
-  }),
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+    prompt: "consent",
+  })
 );
 
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "../google/success",
+    failureRedirect: "../google/failure",
+  })
+);
+
+router.get("/google/success", (req, res) => {
+  console.log(req.session);
+  jwt.sign(
+    {
+      _id: req.user._id,
+      admin: req.user.admin,
+    },
+    JWT_SECRET,
+    { expiresIn: "1h" },
+    (err, token) => {
+      if (err) {
+        return res.json({
+          token: null,
+        });
+      }
+      return res.status(200).json(token);
+    }
+  );
+});
+
+/*
 router.get(
   '/google/callback',
   passport.authenticate('google', {
     session: false,
     failureRedirect: process.env.REGISTER_URL,
     failureMessage: true,
+    successRedirect: '/',
   }),
   (req, res) => {
     jwt.sign(
@@ -61,5 +90,6 @@ router.get(
     );
   },
 );
+*/
 
 module.exports = router;
